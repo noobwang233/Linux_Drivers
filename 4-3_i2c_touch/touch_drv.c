2cd266fa794ea497108f7b00535c6a4e43d90b6b
noobwang233 <1213236870@qq.com>
2024-06-05 17:08:01 +0800

    save

--- 4-3_i2c_touch/touch_drv.c
+++ 4-3_i2c_touch/touch_drv.c
@@ -14,19 +14,19 @@
 #define MAX_SUPPORT_POINTS 5 /* 5点触摸 */
 
 /* TOUCH IC寄存器相关宏定义 */
-#define GT_CTRL_REG             0X8040  /* GT9147控制寄存器         */
-#define GT_MODSW_REG             0X804D  /* GT9147模式切换寄存器        */
-#define GT_CFGS_REG             0X8047  /* GT9147配置起始地址寄存器    */
-#define GT1151_CFGS_REG         0X8050  /* GT1151配置起始地址寄存器    */
-#define GT_CHECK_REG             0X80FF  /* GT9147校验和寄存器       */
-#define GT_PID_REG                 0X8140  /* GT9147产品ID寄存器       */
+#define GT_CTRL_REG                 0X8040  /* GT9147控制寄存器         */
+#define GT_MODSW_REG                0X804D  /* GT9147模式切换寄存器        */
+#define GT_CFGS_REG                 0X8047  /* GT9147配置起始地址寄存器    */
+#define GT1151_CFGS_REG             0X8050  /* GT1151配置起始地址寄存器    */
+#define GT_CHECK_REG                0X80FF  /* GT9147校验和寄存器       */
+#define GT_PID_REG                  0X8140  /* GT9147产品ID寄存器       */
 
-#define GT_GSTID_REG             0X814E  /* GT9147当前检测到的触摸情况 */
-#define GT_TP1_REG                 0X8150  /* 第一个触摸点数据地址 */
-#define GT_TP2_REG                 0X8158    /* 第二个触摸点数据地址 */
-#define GT_TP3_REG                 0X8160  /* 第三个触摸点数据地址 */
-#define GT_TP4_REG                 0X8168  /* 第四个触摸点数据地址  */
-#define GT_TP5_REG                 0X8170    /* 第五个触摸点数据地址   */
+#define GT_GSTID_REG                0X814E  /* GT9147当前检测到的触摸情况 */
+#define GT_TP1_REG                  0X8150  /* 第一个触摸点数据地址 */
+#define GT_TP2_REG                  0X8158  /* 第二个触摸点数据地址 */
+#define GT_TP3_REG                  0X8160  /* 第三个触摸点数据地址 */
+#define GT_TP4_REG                  0X8168  /* 第四个触摸点数据地址  */
+#define GT_TP5_REG                  0X8170  /* 第五个触摸点数据地址   */
 
 #define GT9147_XYCOORDREG_NUM    30        /* 触摸点坐标寄存器数量 */
 
@@ -354,10 +354,10 @@ static int i2c_touch_drv_probe(struct i2c_client *client, const struct i2c_devic
     __set_bit(EV_ABS, dev->inputdev->evbit);
     __set_bit(BTN_TOUCH, dev->inputdev->keybit);
 
-    input_set_abs_params(dev->inputdev, ABS_X, 0, 480, 0, 0);
-    input_set_abs_params(dev->inputdev, ABS_Y, 0, 272, 0, 0);
-    input_set_abs_params(dev->inputdev, ABS_MT_POSITION_X,0, 480, 0, 0);
-    input_set_abs_params(dev->inputdev, ABS_MT_POSITION_Y,0, 272, 0, 0);	     
+    input_set_abs_params(dev->inputdev, ABS_X, 0, 800, 0, 0);
+    input_set_abs_params(dev->inputdev, ABS_Y, 0, 480, 0, 0);
+    input_set_abs_params(dev->inputdev, ABS_MT_POSITION_X,0, 800, 0, 0);
+    input_set_abs_params(dev->inputdev, ABS_MT_POSITION_Y,0, 480, 0, 0);	     
     retvalue = input_mt_init_slots(dev->inputdev, MAX_SUPPORT_POINTS, 0);
     if (retvalue) {
         goto fail;
