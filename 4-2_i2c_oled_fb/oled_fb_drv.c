86c73f56da2ad37e216ac2eccff7222c078ae2e9
noobwang233 <1213236870@qq.com>
2024-06-18 17:21:23 +0800

    save

--- 4-2_i2c_oled_fb/oled_fb_drv.c
+++ 4-2_i2c_oled_fb/oled_fb_drv.c
@@ -194,49 +194,17 @@ static int sh1106_fb_update_display(struct sh1106_fb_par *par)
 {
     struct sh1106_fb_array *array;
     u8 *vmem = par->info->screen_base;
-
-    unsigned int line_length = par->info->fix.line_length;
     int pages = DIV_ROUND_UP(par->height, 8);
     int i, j, k;
+    unsigned int line_length = par->info->fix.line_length;
 
-    printk("===========%s %d=============\n", __FUNCTION__, __LINE__);
-
-    /*
-    * The screen is divided in pages, each having a height of 8
-    * pixels, and the width of the screen. When sending a byte of
-    * data to the controller, it gives the 8 bits for the current
-    * column. I.e, the first byte are the 8 bits of the first
-    * column, then the 8 bits for the second column, etc.
-    *
-    *
-    * Representation of the screen, assuming it is 5 bits
-    * wide. Each letter-number combination is a bit that controls
-    * one pixel.
-    *
-    * A0 A1 A2 A3 A4
-    * B0 B1 B2 B3 B4
-    * C0 C1 C2 C3 C4
-    * D0 D1 D2 D3 D4
-    * E0 E1 E2 E3 E4
-    * F0 F1 F2 F3 F4
-    * G0 G1 G2 G3 G4
-    * H0 H1 H2 H3 H4
-    *
-    * If you want to update this screen, you need to send 5 bytes:
-    *  (1) A0 B0 C0 D0 E0 F0 G0 H0
-    *  (2) A1 B1 C1 D1 E1 F1 G1 H1
-    *  (3) A2 B2 C2 D2 E2 F2 G2 H2
-    *  (4) A3 B3 C3 D3 E3 F3 G3 H3
-    *  (5) A4 B4 C4 D4 E4 F4 G4 H4
-    */
-
+    array = sh1106_fb_alloc_array(par->width, SH1106_DATA);
+    if (!array)
+        return -1;
+    
+    printk("s\n");
     for (i = 0; i < pages; i++)
     {
-        array = sh1106_fb_alloc_array(par->width,
-                        SH1106_DATA);
-        if (!array)
-            return -1;
-
         sh1106_page_set(par->client, i);
         sh1106_column_set(par->client, 0);
 
@@ -252,9 +220,9 @@ static int sh1106_fb_update_display(struct sh1106_fb_par *par)
             }
         }
         sh1106_fb_write_array(par->client, array, par->width);
-        kfree(array);
     }
 
+    kfree(array);
     return 0;
 }
 
@@ -272,7 +240,6 @@ static ssize_t sh1106_fb_write(struct fb_info *info, const char __user *buf,
     unsigned long p = *ppos;
     u8 __iomem *dst;
 
-    printk("===========%s %d=============\n", __FUNCTION__, __LINE__);
     total_size = info->fix.smem_len;
 
     if (p > total_size)
@@ -350,7 +317,6 @@ static int sh1106_init(struct sh1106_fb_par *par)
     const uint8_t *InitCmd = sh1106_InitCmd;
     const uint8_t len = sizeof(sh1106_InitCmd) / sizeof(sh1106_InitCmd[0]);
 
-    printk("===========%s %d=============\n", __FUNCTION__, __LINE__);
     for(i = 0; i < len; i++)
     {
         ret = sh1106_fb_write_cmd(par->client, InitCmd[i]);
@@ -375,8 +341,6 @@ static int oled_drv_probe(struct i2c_client *client, const struct i2c_device_id 
     u8 *vmem;
     int ret;
     struct fb_deferred_io *sh1106_fb_defio;
-
-    printk("===========%s %d=============\n", __FUNCTION__, __LINE__);
 
     if (!node) {
         dev_err(&client->dev, "No device tree data found!\n");
@@ -467,13 +431,12 @@ static int oled_drv_probe(struct i2c_client *client, const struct i2c_device_id 
         dev_err(&client->dev, "Couldn't register the framebuffer\n");
         goto fb_alloc_error;
     }
-    dev_info(&client->dev, "fb%d: %s framebuffer device registered, using %d bytes of video memory\n", info->node, info->fix.id, vmem_size);
-    printk("==========%s success =======\n", __FUNCTION__);
+
     return 0;
 
 fb_alloc_error:
     framebuffer_release(info);
-    printk("==========%s failed =======\n", __FUNCTION__);
+
     return ret;
 
 }
@@ -487,8 +450,6 @@ static int oled_drv_remove(struct i2c_client *client)
 {
     struct fb_info *info = i2c_get_clientdata(client);
 
-    printk("===========%s %d=============\n", __FUNCTION__, __LINE__);
-
 	sh1106_fb_write_cmd(client, SH1106_DISPLAY_OFF);
 
     unregister_framebuffer(info);
@@ -499,7 +460,6 @@ static int oled_drv_remove(struct i2c_client *client)
 
     framebuffer_release(info);
 
-    printk("==========%s success =======\n", __FUNCTION__);
     return 0;
 }
 
@@ -541,10 +501,10 @@ static int __init oled_drv_init(void)
 
     //注册i2c_driver
     retvalue = i2c_add_driver(&oled_i2c_drv);
-    if(!retvalue)
-        printk("===========%s successfully =============\n", __FUNCTION__);
-    else
-        printk("===========%s failed =============\n", __FUNCTION__);
+    // if(!retvalue)
+    //     // printk("===========%s successfully =============\n", __FUNCTION__);
+    // else
+    //     // printk("===========%s failed =============\n", __FUNCTION__);
     return retvalue;
 }
 
@@ -558,7 +518,7 @@ static void __exit oled_drv_exit(void)
 {
     // 注销i2c_driver
     i2c_del_driver(&oled_i2c_drv);
-    printk("class_destroy success!\n");
+    // printk("class_destroy success!\n");
 }
 
 module_init(oled_drv_init);
